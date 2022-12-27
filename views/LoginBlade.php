4cbc9e4a034eb62aee75884d57865fbcc3346e41
code et motivation <judarmwil@gmail.com>
2022-12-27 22:47:29 +0100

    mise en place du code html de la vue LoginBlade

---
+++ views/LoginBlade.php
@@ -0,0 +1,46 @@
+<?php 
+
+include "../views/layouts/EnteteBlade.php";
+
+?>
+
+
+<div class="wrapper">
+   	<section class="form login">
+   		<header>
+   			CONNEXION 
+   		</header>
+   		<form method="post" action="">
+   			<div class="error-txt">
+   				
+   			</div>
+   			<div class="name-details">
+   				<div class="field input">
+   					<label>email address</label>
+   					<input type="text" name="email" placeholder="email address">
+   				</div>
+   				<div class="field input">
+   					<label>password</label>
+   					<input type="password" name="password" placeholder="password">
+                  	<i class="fas fa-eye"></i>
+   				</div>
+   				<div class="field button">
+   					<input type="submit" value="Se Connecter">
+   				</div>
+   				<div class="link">
+   					Avez Vous un Compte ? <a href="">Creer Maintenant</a>
+   				</div>	
+   			</div>
+   		</form>
+   	</section>
+   </div>
+
+
+
+
+
+<?php
+
+include "../views/layouts/EnteteBlade.php";
+
+ ?>
